ac487ea103f570a00654f1866e87b9b4e33e0b73
saimum <saimum720@gmail.com>
2017-11-15 16:30:33 -0800

    Warehouse crud

--- ERP_Compact/Controllers/MgtWarehouseController.cs
+++ ERP_Compact/Controllers/MgtWarehouseController.cs
@@ -10,21 +10,19 @@ using ERP_Compact.Models;
 
 namespace ERP_Compact.Controllers
 {
-    public class MgtCompanyController : BaseController
+    public class MgtWarehouseController : BaseController
     {
-
         public ActionResult Index()
         {
-            var companyViewModelList = db.Company.Where(x=>x.IsDelete==false).Select(model => new CompanyViewModel()
-            {
-                CompanyKey = model.CompanyKey,
-                CompanyName = model.CompanyName,
-                CompanyPhone = model.CompanyPhone,
-                CompanyAddress = model.CompanyAddress,
-                CompanyEmail = model.CompanyEmail
+            var warehouseList = db.Warehouse.Where(x=>x.IsDelete==false).Select(model=> new WarehouseViewModel() {
+                WarehouseKey=model.WarehouseKey,
+                WahouseName=model.WahouseName,
+                WahousePhone=model.WahousePhone,
+                WahouseAddress=model.WahouseAddress,
+                WahouseEmail=model.WahouseEmail
+
             }).ToList();
-            
-            return View(companyViewModelList);
+            return View(warehouseList);
         }
 
         public ActionResult Details(Guid? id)
@@ -33,24 +31,24 @@ namespace ERP_Compact.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            var model = db.Company.Where(x=>x.CompanyKey == id).Include(x => x.Division).Include(x => x.District).Include(x => x.Upazilla).FirstOrDefault();
+            var model = db.Warehouse.Where(x => x.WarehouseKey == id).Include(x => x.Division).Include(x => x.District).Include(x => x.Upazilla).FirstOrDefault();
             if (model == null)
             {
                 return HttpNotFound();
             }
             else
             {
-                var viewModel = new CompanyViewModel()
+                var viewModel = new WarehouseViewModel()
                 {
-                    CompanyKey = model.CompanyKey,
-                    CompanyName = model.CompanyName,
-                    CompanyID = model.CompanyID,
-                    CompanyAddress = model.CompanyAddress,
-                    CompanyPhone = model.CompanyPhone,
-                    CompanyMobile = model.CompanyMobile,
-                    CompanyEmail = model.CompanyEmail,
-                    CompanyWebsite = model.CompanyWebsite,
-                    CompanyFax = model.CompanyFax,
+                    WarehouseKey = model.WarehouseKey,
+                    WahouseID = model.WahouseID,
+                    WahouseName = model.WahouseName,
+                    WahouseAddress = model.WahouseAddress,
+                    WahousePhone = model.WahousePhone,
+                    WahouseMobile = model.WahouseMobile,
+                    WahouseEmail = model.WahouseEmail,
+                    WahouseWebsite = model.WahouseWebsite,
+                    WahouseFax = model.WahouseFax,
                     ContactPersonName = model.ContactPersonName,
                     ContactPersonNo = model.ContactPersonNo,
                     Logo = model.Logo,
@@ -58,14 +56,15 @@ namespace ERP_Compact.Controllers
                 };
 
                 viewModel.DivisionName = model.Division != null ? model.Division.DivisionName : "n/a";
-                viewModel.DistrictName= model.District != null ? model.District.DistrictName : "n/a";
-                viewModel.UpazillaName= model.Upazilla != null ? model.Upazilla.UpazillaName : "n/a";
+                viewModel.DistrictName = model.District != null ? model.District.DistrictName : "n/a";
+                viewModel.UpazillaName = model.Upazilla != null ? model.Upazilla.UpazillaName : "n/a";
 
                 return View(viewModel);
             }
-            
+
         }
 
+        // GET: MgtWarehouse/Create
         public ActionResult Create()
         {
             ViewBag.DivisionKey = new SelectList(db.Division.Where(x => x.IsDelete == false), "DivisionKey", "DivisionID");
@@ -74,44 +73,46 @@ namespace ERP_Compact.Controllers
             return View();
         }
 
-
+        // POST: MgtWarehouse/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Exclude = "Logo")] CompanyViewModel viewModel, HttpPostedFileBase Logo)
+        public ActionResult Create([Bind(Exclude = "Logo")] WarehouseViewModel viewModel, HttpPostedFileBase Logo)
         {
             if (ModelState.IsValid)
             {
                 try
                 {
-                    Company model = new Company();
+                    Warehouse model = new Warehouse();
                     model.IsDelete = false;
-                    model.CompanyKey = Guid.NewGuid();
+                    model.WarehouseKey = Guid.NewGuid();
 
-                    model.CompanyName = viewModel.CompanyName;
-                    model.CompanyID = string.IsNullOrEmpty(viewModel.CompanyID) ? viewModel.CompanyName : viewModel.CompanyID;
-                    model.CompanyAddress = viewModel.CompanyAddress;
-                    model.CompanyPhone = string.IsNullOrEmpty(viewModel.CompanyPhone) ? "n/a" : viewModel.CompanyPhone;
-                    model.CompanyMobile = string.IsNullOrEmpty(viewModel.CompanyMobile) ? "n/a" : viewModel.CompanyMobile;
-                    model.CompanyEmail = string.IsNullOrEmpty(viewModel.CompanyEmail) ? "n/a" : viewModel.CompanyEmail;
-                    model.CompanyWebsite = string.IsNullOrEmpty(viewModel.CompanyWebsite) ? "n/a" : viewModel.CompanyWebsite;
-                    model.CompanyFax = string.IsNullOrEmpty(viewModel.CompanyFax) ? "n/a" : viewModel.CompanyFax;
+                    model.WahouseName = viewModel.WahouseName;
+                    model.WahouseID = string.IsNullOrEmpty(viewModel.WahouseID) ? viewModel.WahouseName : viewModel.WahouseID;
+                    model.WahouseAddress = viewModel.WahouseAddress;
+                    model.WahousePhone = string.IsNullOrEmpty(viewModel.WahousePhone) ? "n/a" : viewModel.WahousePhone;
+                    model.WahouseMobile = string.IsNullOrEmpty(viewModel.WahouseMobile) ? "n/a" : viewModel.WahouseMobile;
+                    model.WahouseEmail = string.IsNullOrEmpty(viewModel.WahouseEmail) ? "n/a" : viewModel.WahouseEmail;
+                    model.WahouseWebsite = string.IsNullOrEmpty(viewModel.WahouseWebsite) ? "n/a" : viewModel.WahouseWebsite;
+                    model.WahouseFax = string.IsNullOrEmpty(viewModel.WahouseFax) ? "n/a" : viewModel.WahouseFax;
                     model.ContactPersonName = string.IsNullOrEmpty(viewModel.ContactPersonName) ? "n/a" : viewModel.ContactPersonName;
                     model.ContactPersonNo = string.IsNullOrEmpty(viewModel.ContactPersonNo) ? "n/a" : viewModel.ContactPersonNo;
                     model.DivisionKey = viewModel.DivisionKey;
                     model.DistrictKey = viewModel.DistrictKey;
                     model.UpazillaKey = viewModel.UpazillaKey;
-                    
+
                     byte[] imgBinaryData = new byte[Logo.ContentLength];
                     int readresult = Logo.InputStream.Read(imgBinaryData, 0, Logo.ContentLength);
                     model.Logo = imgBinaryData;
                     model.LogoType = Logo.ContentType;
-                    db.Company.Add(model);
+                    db.Warehouse.Add(model);
                     db.SaveChanges();
-                    RenderSuccessMessage("Company is successfully created.");
+                    RenderSuccessMessage("Warehouse is successfully created.");
                 }
                 catch (Exception e)
                 {
-                    RenderDangerMessage("Company could not be created due to an error.");
+                    RenderDangerMessage("Warehouse could not be created due to an error.");
                 }
 
                 return RedirectToAction("Index");
@@ -123,26 +124,27 @@ namespace ERP_Compact.Controllers
                 ViewBag.UpazillaKey = new SelectList(db.Upazilla.Where(x => x.IsDelete == false && x.DistrictKey == viewModel.DistrictKey), "UpazillaKey", "UpazillaID", viewModel.UpazillaKey);
                 return View(viewModel);
             }
-            
+
         }
 
+        // GET: MgtWarehouse/Edit/5
         public ActionResult Edit(Guid? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            var viewModel = db.Company.Where(x => x.CompanyKey == id).Select(model => new CompanyViewModel()
+            var viewModel = db.Warehouse.Where(x => x.WarehouseKey == id).Select(model => new WarehouseViewModel()
             {
-                CompanyKey = model.CompanyKey,
-                CompanyName = model.CompanyName,
-                CompanyID = model.CompanyID,
-                CompanyAddress = model.CompanyAddress,
-                CompanyPhone = model.CompanyPhone,
-                CompanyMobile = model.CompanyMobile,
-                CompanyEmail = model.CompanyEmail,
-                CompanyWebsite = model.CompanyWebsite,
-                CompanyFax = model.CompanyFax,
+                WarehouseKey = model.WarehouseKey,
+                WahouseName = model.WahouseName,
+                WahouseID = model.WahouseID,
+                WahouseAddress = model.WahouseAddress,
+                WahousePhone = model.WahousePhone,
+                WahouseMobile = model.WahouseMobile,
+                WahouseEmail= model.WahouseEmail,
+                WahouseWebsite = model.WahouseWebsite,
+                WahouseFax= model.WahouseFax,
                 ContactPersonName = model.ContactPersonName,
                 ContactPersonNo = model.ContactPersonNo,
                 DivisionKey = model.DivisionKey,
@@ -163,25 +165,27 @@ namespace ERP_Compact.Controllers
             return View(viewModel);
         }
 
+        // POST: MgtWarehouse/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Exclude = "Logo")] CompanyViewModel viewModel, HttpPostedFileBase Logo)
-
+        public ActionResult Edit([Bind(Exclude = "Logo")] WarehouseViewModel viewModel, HttpPostedFileBase Logo)
         {
             if (ModelState.IsValid)
             {
                 try
                 {
-                    Company model = db.Company.Where(x => x.CompanyKey == viewModel.CompanyKey).FirstOrDefault();
+                    Warehouse model = db.Warehouse.Where(x => x.WarehouseKey== viewModel.WarehouseKey).FirstOrDefault();
 
-                    model.CompanyName = viewModel.CompanyName;
-                    model.CompanyID = string.IsNullOrEmpty(viewModel.CompanyID) ? viewModel.CompanyName : viewModel.CompanyID;
-                    model.CompanyAddress = viewModel.CompanyAddress;
-                    model.CompanyPhone = string.IsNullOrEmpty(viewModel.CompanyPhone) ? "n/a" : viewModel.CompanyPhone;
-                    model.CompanyMobile = string.IsNullOrEmpty(viewModel.CompanyMobile) ? "n/a" : viewModel.CompanyMobile;
-                    model.CompanyEmail = string.IsNullOrEmpty(viewModel.CompanyEmail) ? "n/a" : viewModel.CompanyEmail;
-                    model.CompanyWebsite = string.IsNullOrEmpty(viewModel.CompanyWebsite) ? "n/a" : viewModel.CompanyWebsite;
-                    model.CompanyFax = string.IsNullOrEmpty(viewModel.CompanyFax) ? "n/a" : viewModel.CompanyFax;
+                    model.WahouseName = viewModel.WahouseName;
+                    model.WahouseID = string.IsNullOrEmpty(viewModel.WahouseID) ? viewModel.WahouseName : viewModel.WahouseID;
+                    model.WahouseAddress = viewModel.WahouseAddress;
+                    model.WahousePhone = string.IsNullOrEmpty(viewModel.WahousePhone) ? "n/a" : viewModel.WahousePhone;
+                    model.WahouseMobile= string.IsNullOrEmpty(viewModel.WahouseMobile) ? "n/a" : viewModel.WahouseMobile;
+                    model.WahouseEmail= string.IsNullOrEmpty(viewModel.WahouseEmail) ? "n/a" : viewModel.WahouseEmail;
+                    model.WahouseWebsite= string.IsNullOrEmpty(viewModel.WahouseWebsite) ? "n/a" : viewModel.WahouseWebsite;
+                    model.WahouseFax= string.IsNullOrEmpty(viewModel.WahouseFax) ? "n/a" : viewModel.WahouseFax;
                     model.ContactPersonName = string.IsNullOrEmpty(viewModel.ContactPersonName) ? "n/a" : viewModel.ContactPersonName;
                     model.ContactPersonNo = string.IsNullOrEmpty(viewModel.ContactPersonNo) ? "n/a" : viewModel.ContactPersonNo;
                     model.DivisionKey = viewModel.DivisionKey;
@@ -197,18 +201,18 @@ namespace ERP_Compact.Controllers
                     }
                     db.SaveChanges();
 
-                    RenderSuccessMessage("Company is successfully updated.");
+                    RenderSuccessMessage("Wahouse is successfully updated.");
                     return RedirectToAction("Index");
                 }
                 catch (Exception e)
                 {
-                    RenderDangerMessage("Company could not be updated due to an error.");
-                    return RedirectToAction("Edit", new { id = viewModel.CompanyKey });
+                    RenderDangerMessage("Wahouse could not be updated due to an error.");
+                    return RedirectToAction("Edit", new { id = viewModel.WarehouseKey });
                 }
             }
             else
             {
-                viewModel.Logo = db.Company.Where(x => x.CompanyKey == viewModel.CompanyKey).Select(x => x.Logo).FirstOrDefault();
+                viewModel.Logo = db.Company.Where(x => x.CompanyKey == viewModel.WarehouseKey).Select(x => x.Logo).FirstOrDefault();
                 ViewBag.DivisionKey = new SelectList(db.Division.Where(x => x.IsDelete == false), "DivisionKey", "DivisionID", viewModel.DivisionKey);
                 ViewBag.DistrictKey = new SelectList(db.District.Where(x => x.IsDelete == false && x.DivisionKey == viewModel.DivisionKey), "DistrictKey", "DistrictID", viewModel.DistrictKey);
                 ViewBag.UpazillaKey = new SelectList(db.Upazilla.Where(x => x.IsDelete == false && x.DistrictKey == viewModel.DistrictKey), "UpazillaKey", "UpazillaID", viewModel.UpazillaKey);
@@ -216,17 +220,18 @@ namespace ERP_Compact.Controllers
                 RenderInfoMessage("Please, provide all required data.");
                 return View(viewModel);
             }
-            
+
         }
 
+        // GET: MgtWarehouse/Delete/5
         public ActionResult Delete(Guid? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            
-            var model = db.Company.Find(id);
+
+            var model = db.Warehouse.Find(id);
             if (model == null)
             {
                 return HttpNotFound();
@@ -236,17 +241,16 @@ namespace ERP_Compact.Controllers
             {
                 model.IsDelete = true;
                 db.SaveChanges();
-                RenderSuccessMessage("Company is successfully deleted.");
+                RenderSuccessMessage("Warehouse is successfully deleted.");
                 return RedirectToAction("Index");
             }
             catch (Exception)
             {
-                RenderDangerMessage("Company could not be deleted due to an error.");
+                RenderDangerMessage("Warehouse could not be deleted due to an error.");
                 return RedirectToAction("Index");
             }
-            
-        }
 
+        }
 
         protected override void Dispose(bool disposing)
         {
